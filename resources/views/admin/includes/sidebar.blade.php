7f6dcf21a25a332217941cdaf56a69e38816c606
San4a <racing-taz@e1.ru>
2022-05-28 10:43:35 +0500

    ADD sidebar

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,25 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+        <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+             style="opacity: .8">
+        <span class="brand-text font-weight-light">Blog</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+            data-accordion="false">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th-list"></i>
+                    <p>
+                        Категории
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- /.sidebar -->
+</aside>
