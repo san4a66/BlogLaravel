3117beb5414582f77d944c123c9b19f39ea365a9
San4a <racing-taz@e1.ru>
2022-06-08 17:13:42 +0500

    ADD Breadcrumbs

--- app/Http/Controllers/Admin/User/ShowController.php
+++ app/Http/Controllers/Admin/User/ShowController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\User;
 
 
 class ShowController extends Controller
 {
-    public function __invoke(Category $user)
+    public function __invoke(User $user)
     {
         return view('admin.user.show', compact('user'));
     }
