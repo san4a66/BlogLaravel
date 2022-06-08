f65be3fbb64a81a42f1aaab009ec2bb1661118a7
San4a <racing-taz@e1.ru>
2022-06-08 17:58:07 +0500

    ADD PersonalCab

---
+++ resources/views/personal/includes/sidebar.blade.php
@@ -0,0 +1,41 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="{{route('admin.main.index')}}" class="brand-link">
+        <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+             style="opacity: .8">
+        <span class="brand-text font-weight-light">Blog</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+            data-accordion="false">
+            <li class="nav-item">
+                <a href="{{route('personal.main.index')}}" class="nav-link">
+                    <i class="nav-icon fas fa-home"></i>
+                    <p>
+                        Главная
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{route('personal.liked.index')}}" class="nav-link">
+                    <i class="nav-icon fas fa-heart"></i>
+                    <p>
+                        Понравившиеся посты
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{route('personal.comment.index')}}" class="nav-link">
+                    <i class="nav-icon fas fa-comment"></i>
+                    <p>
+                        Комментарии
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- /.sidebar -->
+</aside>
