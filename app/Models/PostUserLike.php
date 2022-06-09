4f86484134f91b80549851700bd37bd16a55ca76
San4a <racing-taz@e1.ru>
2022-06-09 12:16:23 +0500

    Create table PostUserLikes

--- app/Models/PostUserLike.php
+++ app/Models/PostUserLike.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PostTag extends Model
+class PostUserLike extends Model
 {
     use HasFactory;
-    protected $table = 'post_tags';
+    protected $table = 'post_user_likes';
     protected $guarded = false;
 }
