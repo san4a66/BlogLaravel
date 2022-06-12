b300a2ec9288796dcd2c58b0cdb5908be00e4433
San4a <racing-taz@e1.ru>
2022-06-12 12:23:18 +0500

    ADD PostShow

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -10,9 +10,6 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        $posts = Post::paginate(6);
-        $randomPosts = Post::get()->random(4);
-        $popularPosts = Post::withCount('likedUsers')->orderBy('liked_users_count','DESC')->get()->take(4);
-        return view('main.index', compact('posts','randomPosts', 'popularPosts'));
+        return redirect()->route('post.index');
     }
 }
